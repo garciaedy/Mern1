defb5ded454ea1dafdbc951df7a6f270bb1e5237
Edy Garcia <edygarcia@edys-imac.attlocal.net>
2019-10-17 23:14:43 -0400

    defined routes

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get('/',(req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
