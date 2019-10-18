0cc95b6876f829b649beb02f944116cde5ae3f04
Edy Garcia <edygarcia@edys-imac.attlocal.net>
2019-10-17 22:44:18 -0400

    setting up routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,10 @@
 const express = require('espress');
 const router = express.Router();
 
+// @route GET api/users
+//  @dec Test route
+// @access public
 router.get('/', (req, res) => res.send('User router')); 
+
+
+module.exports = rputer;
