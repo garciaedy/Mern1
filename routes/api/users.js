2fee29c636b769487801bd0f34f431f9596ca724
Edy Garcia <edygarcia@edys-imac.attlocal.net>
2019-10-21 17:21:41 -0400

    used async to check user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 const {check, validationResult} = require('express-validator/check');
 
 // Model
@@ -20,22 +21,30 @@ router.post(
         'please enter a password with 6 or more characters'
     ).isLength({min: 6})
 ], 
-(req, res) => {
+ async(req, res) => {
 const errors = validationResult(req);
 if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
 }
 
+const {name, email, password} = req.body;
 
+try{
 
 // See if the user exist
+let user = await User.findOne({email});
+
+if(user){
+    res.status(400).json({errors: [{msg: 'User already exist'}]});
+}
 // GET Users gravatar 
 // Encrypt password usind bcrypt
 // Return jsonwebtoken
-
-
-
 res.send('User router');
+}catch(err){
+console.error(err.message);
+res.status(500).send('Server error')
+}
 
 }); 
 
