ecaf6343300ecc14120747e60da8063bafa81ab5
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-10-22 23:59:26 -0400

    made call to db and brought model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth.js')
+const User = require('../../models/User')
 
 // @route GET api/auth
 //  @dec Test route
 // @access public
-router.get('/', (req, res) => res.send('auth router')); 
+router.get('/', auth, async (req, res) => {
+try {
+const user = await User.findById(req.user.id)
+}catch(err){
+
+}
+
+});
+
 
 
 module.exports = router;
