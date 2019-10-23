037227cbb9a26ad84f527dea9f1d91e0b746ff44
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-10-23 18:32:18 -0400

    added validation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,76 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth.js')
 const User = require('../../models/User')
+const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator/check');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+
+
+// @route POST api/users
+//  @dec  register user
+// @access public
+router.post(
+    '/',
+    [
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+        'password',
+        'password is required'
+    ).exists()
+],
+ async(req, res) => {
+const errors = validationResult(req);
+if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+}
+
+const {email, password} = req.body;
+
+try{
+
+// See if the user exist
+let user = await User.findOne({email});
+
+if(!user){
+    return res
+    .status(400)
+    .json({errors: [{msg: 'ivalid credetials'}]});
+}
+// Return jsonwebtoken
+// res.send('User registered');
+
+const isMatch = await bcrypt.compare(password, user.password);
+
+if(!isMatch){
+  return res
+  .status(400)
+  .json({errors: [{msg: 'ivalid credetials'}]});
+}
+
+
+const payload ={
+  user: {
+    id: user.id
+  }
+};
+
+jwt.sing(
+  payload,
+   config.get('jwtSecret'),
+ {expiresIn: 36000}),
+ (err, token) =>{
+if(err) throw err;
+res.json({token});
+ }
+
+}catch(err){
+console.error(err.message);
+res.status(500).send('Server error')
+}
+
+});
+
 
 // @route GET api/auth
 //  @dec Test route
